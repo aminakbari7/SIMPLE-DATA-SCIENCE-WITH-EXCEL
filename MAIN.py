f2bb3fbc9d6ce8694ea85b63706284fc8d47e691
aminakbari7 <aminakbari733@gmail.com>
2024-03-01 15:33:30 +0330

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,8 +1,8 @@
 import pandas as pd
-
-
+import numpy as np
+import matplotlib.pyplot as plt
 class Analysis_Data:
-    def __init__(self,dataframe, ):
+    def __init__(self,dataframe):
           self.dataframe = dataframe
           self.Full_Names=dataframe['Full Name']
           self.Job_Titles=dataframe['Job Title']
@@ -17,14 +17,23 @@ class Analysis_Data:
           self.Countrys=dataframe['Country']
           self.Citys=dataframe['City']
     def full_name_of_max_salary(self):
-       return self.Full_Names[self.Annual_Salarys.idxmax()],
-
+       return self.Full_Names[self.Annual_Salarys.idxmax()]
+    def full_name_of_min_salary(self):
+       return self.Full_Names[self.Annual_Salarys.idxmin()]
+    def diplay2dplot(self):
+            npx=np.asarray(self.Full_Names)
+            npy=np.asarray(self.Genders)
+            plt.plot(npx,npy)
+            plt.show()
 def main():
     dataframe = pd.read_excel('data.xlsx')
     My_Analysis_Data=Analysis_Data(dataframe= dataframe)
     
-    #####-> who get most salary?
-    print(My_Analysis_Data.full_name_of_max_salary())
     
+    #print("min salary = ",My_Analysis_Data.full_name_of_min_salary(),My_Analysis_Data.Annual_Salarys.min())
+    #print("max salary = ",My_Analysis_Data.full_name_of_max_salary(),My_Analysis_Data.Annual_Salarys.max())
+    ###->plot two of header list
+    My_Analysis_Data.diplay2dplot()
+  
 if __name__=="__main__":
     main()
